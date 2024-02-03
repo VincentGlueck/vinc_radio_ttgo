bbb6871629507f93d764cca3bfcfb0a013478d0c
Martin Lange <martin.h.lange@gmail.com>
2024-02-03 12:54:05 +0100

    Pixel shifting, scroll speed, char size only

--- Internetradio/InternetRadio/stations.h
+++ Internetradio/InternetRadio/stations.h
@@ -5,11 +5,11 @@ struct Station {
 };
 
 const Station stations[] = {
-  {"NDR Enjoy", "http://icecast.ndr.de/ndr/njoy/live/mp3/128/stream.mp3", 4.0f},
-  {"Sunshine LIVE", "http://stream.sunshine-live.de/live/mp3-192", 3.0f},
-  {"Sunshine House", "http://stream.sunshine-live.de/house/mp3-192", 3.0f},
-  {"Sunshine Lounge", "http://stream.sunshine-live.de/lounge/mp3-192", 3.0f},
-  {"NDR 2", "http://icecast.ndr.de/ndr/ndr2/hamburg/mp3/128/stream.mp3", 4.0f},
-  {"NDR 1 NDS", "http://icecast.ndr.de/ndr/ndr1niedersachsen/hannover/mp3/128/stream.mp3", 4.0f},
-  {"1LIVE", "http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3", 4.0f}
+  {"NDR Enjoy", "http://icecast.ndr.de/ndr/njoy/live/mp3/128/stream.mp3", 7.0f},
+  {"Sunshine LIVE", "http://stream.sunshine-live.de/live/mp3-192", 6.0f},
+  {"Sunshine House", "http://stream.sunshine-live.de/house/mp3-192", 6.0f},
+  {"Sunshine Lounge", "http://stream.sunshine-live.de/lounge/mp3-192", 6.0f},
+  {"NDR 2", "http://icecast.ndr.de/ndr/ndr2/hamburg/mp3/128/stream.mp3", 7.0f},
+  {"NDR 1 NDS", "http://icecast.ndr.de/ndr/ndr1niedersachsen/hannover/mp3/128/stream.mp3", 7.0f},
+  {"1LIVE", "http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3", 7.0f}
 };
